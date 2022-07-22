bc08d6c81bf69e845e21c156ba4fe6e5a6a9261a
Arya <singasanearya@gmail.com>
2022-07-22 16:44:46 +0530

    search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 function SearchBar() {
   return (
-    
     <div className="search-wrapper">
         <input className="w-44 m-2 p-2 cursor-pointer rounded-md focus:outline-none" type="text" placeholder="Search" />
         <svg xmlns="http://www.w3.org/2000/svg" width="10" height="20" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="feather feather-search w-10" viewBox="0 0 25 25">
@@ -10,7 +9,6 @@ function SearchBar() {
             <path d="M21 21l-4.35-4.35"></path>
         </svg>
     </div>
-
   )
 }
 
