7b55c19fe5a41efde4aa01ced0b213e4e592f6ad
Yash-Dabhade <yashdabhade215@gmail.com>
2022-07-22 13:13:12 +0530

    Resolved conficts

--- src/testing/TestPage.js
+++ src/testing/TestPage.js
@@ -1,23 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
-import UniversityForm from "../pages/forms/UniversityForm";
-=======
-import React from 'react'
-import SimpleCard from '../components/SimpleCard';
->>>>>>> 476cb1e6b1cecf290951ba3c0232e041b68241db
 
 function TestPage() {
-  return (
-    <>
-<<<<<<< HEAD
-      <div>
-        <UniversityForm />
-      </div>
-=======
-     <SimpleCard title = "Maharashtra State Board of Technical Education"  desc="The MSBTE is an autonomous Board of the Government of Maharashtra. MSBTE designs and implements diploma, post diploma and advanced diploma programs to affiliated institutions." logo={require('../res/MSBTE_logo.png')}/>
->>>>>>> 476cb1e6b1cecf290951ba3c0232e041b68241db
-    </>
-  );
+  return <>Test page</>;
 }
 
 export default TestPage;
