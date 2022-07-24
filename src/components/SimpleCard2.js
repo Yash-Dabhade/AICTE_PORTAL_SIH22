297038d8670c761c89a549be3b1ad9a696da385c
Yash-Dabhade <yashdabhade215@gmail.com>
2022-07-24 17:44:16 +0530

    Modified SimpleCard2

--- src/components/SimpleCard2.js
+++ src/components/SimpleCard2.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-function SimpleCard2() {
+function SimpleCard2(props) {
   return (
     <div className="container flex h-screen justify-center text-lg items-center">
       <div className="content border-2 border-[black] rounded-lg  py-3 w-1/3">
-        <div className="title p-3 m-4 border-b-2 border-[black]">Title</div>
-        <div className="subtitle p-3 m-4">Sub title</div>
+        <div className="title p-3 m-4 border-b-2 border-[black]">
+          {props.title}
+        </div>
+        <div className="subtitle p-3 m-4">{props.subTitle}</div>
         <div className="content2 flex m-4 justify-between p-3">
-          <div className="subjectcode flex-none">Code</div>
+          <div className="subjectcode flex-none">{props.code}</div>
           <div className="view flex rounded-3xl bg-[grey] px-12 py-1">
-            <a href="#" className="btn ">
-              View
-            </a>
+            <button className="btn ">View</button>
           </div>
         </div>
       </div>
