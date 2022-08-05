ffe78dd0b706e25b3bd6e9eb791e9f3bb1f42e3b
Yash-Dabhade <yashdabhade215@gmail.com>
2022-08-05 21:46:24 +0530

    Added new card

--- src/components/NewCard.js
+++ src/components/NewCard.js
@@ -1,14 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function NewCard() {
   return (
-    <div className='card w-card'>
-      <div className='border-2  border-black bg-white w-10 h-10 rounded-lg'>
-        <div className='justify-center items-center text-center text-2xl font-medium'>+
-        </div>
-      </div>
+    <div className="border-2 flex justify-center items-center cursor-pointer hover:shadow-lg border-compatible border-black bg-white  card w-32 h-32 rounded-lg">
+      <div className="text-center text-6xl font-medium">+</div>
     </div>
-  )
+  );
 }
 
-export default NewCard
+export default NewCard;
--- src/pages/TrendingPortal/Trending.js
+++ src/pages/TrendingPortal/Trending.js
@@ -1,10 +1,14 @@
 import React from "react";
 import TrendingIntro from "../../components/TrendingInro";
+import NewCard from "../../components/NewCard";
 
 function Trending() {
   return (
     <div className="parent-section darkMode">
       <TrendingIntro />
+      <div className="flex m-8">
+        <NewCard />
+      </div>
     </div>
   );
 }
