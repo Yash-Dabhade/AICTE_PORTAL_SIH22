62ee5289b2f6854ce8d3d981e085be7dee80c629
Yash-Dabhade <yashdabhade215@gmail.com>
2022-08-06 16:33:07 +0530

    Added loading icon for University

--- src/pages/CurriculumPortal/University.js
+++ src/pages/CurriculumPortal/University.js
@@ -7,10 +7,12 @@ import SubHead from "../../components/SubHead";
 import Header from "../../components/Header";
 import Modal from "react-modal";
 import UniversityForm from "../forms/UniversityForm";
+import HashLoader from "react-spinners/HashLoader";
 
 export default function University(props) {
   const [data, setData] = useState([]);
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   function getAllUniversities() {
     const db = dbref(database);
@@ -23,6 +25,7 @@ export default function University(props) {
             allData.push(data[key]);
           });
           setData(allData);
+          setLoading(false);
         }
       })
       .catch((error) => {
@@ -49,30 +52,53 @@ export default function University(props) {
   }
   return (
     <div className="parent-section" id="formRoot">
-      <Header />
-      <SubHead title={"University"} btnFunc={createUniversity} />
-      <div className="university-boxes jsGridView">
-        {data.map((ele, index) => {
-          return (
-            <Card
-              logo={MSBTE_logo}
-              title={ele.initialName}
-              fullName={ele.fullName}
-              phone={ele.phone}
-              email={ele.email}
-              code={ele.code}
-              location={ele.location}
-              institutes={ele.institutes}
-              website={ele.website}
-              key={index}
-              HiddenRoute={props.HiddenRoute}
+      <div>
+        {loading ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "85vh",
+              width: "100%",
+            }}
+          >
+            <HashLoader
+              color="#0F172A"
+              size={36}
+              margin={2}
+              loading={loading}
             />
-          );
-        })}
+          </div>
+        ) : (
+          <>
+            <Header />
+            <SubHead title={"University"} btnFunc={createUniversity} />
+            <div className="university-boxes jsGridView">
+              {data.map((ele, index) => {
+                return (
+                  <Card
+                    logo={MSBTE_logo}
+                    title={ele.initialName}
+                    fullName={ele.fullName}
+                    phone={ele.phone}
+                    email={ele.email}
+                    code={ele.code}
+                    location={ele.location}
+                    institutes={ele.institutes}
+                    website={ele.website}
+                    key={index}
+                    HiddenRoute={props.HiddenRoute}
+                  />
+                );
+              })}
+            </div>
+            <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+              <UniversityForm btnFunc={closeModal} />
+            </Modal>
+          </>
+        )}
       </div>
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
-        <UniversityForm btnFunc={closeModal} />
-      </Modal>
     </div>
   );
 }
