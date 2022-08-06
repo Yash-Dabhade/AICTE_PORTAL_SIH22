0574c577a578750e3ff7a01fca20a750268b187c
Harshal Tupe <harshaltupe12@gmail.com>
2022-08-06 19:36:42 +0530

    changes done in new Card

--- src/components/NewCard.js
+++ src/components/NewCard.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
+import DetailCard from "../pages/TrendingPortal/DetailCard";
 
 function NewCard() {
+  const [ShowModal, setShowModal] = useState(false);
+  const handleOnClose = () => setShowModal(false);
   return (
-    <div className="border-2 flex justify-center items-center cursor-pointer hover:shadow-lg border-compatible border-black bg-white  card w-32 h-32 rounded-lg">
+    <div onClick={() => setShowModal(true)} className="border-2 flex justify-center items-center cursor-pointer hover:shadow-lg border-compatible border-black bg-white  card w-32 h-32 rounded-lg">
       <div className="text-center text-6xl font-medium">+</div>
+      <DetailCard onClose={handleOnClose} visible={ShowModal}/>
     </div>
   );
 }
