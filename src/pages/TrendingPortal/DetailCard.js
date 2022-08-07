095036c5840c12343ecd6f44fe7430d41e513f0a
Harshal Tupe <harshaltupe12@gmail.com>
2022-08-07 11:57:54 +0530

    created Modal for getting input

--- src/pages/TrendingPortal/DetailCard.js
+++ src/pages/TrendingPortal/DetailCard.js
@@ -24,11 +24,6 @@ export default function DetailCard({onClose,visible}) {
                                 <label for="small-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-700">Enter Record here</label>
                                 <input type="text" id="recordInput" class="block p-2 w-full text-gray-200 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black my-3 dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                         </div>
-                        {/* <button data-modal-toggle="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2" onClick={onClose}>
-                            Yes, I'm sure
-                        </button>
-                        <button data-modal-toggle="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600" onClick={onClose}>No, cancel</button> */}
-
                         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={handelSubmit}>
                         Button
                         </button>
