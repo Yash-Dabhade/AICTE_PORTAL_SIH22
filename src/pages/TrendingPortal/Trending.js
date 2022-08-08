dd451d4047cdbeb233b25743aa62982c4efd515d
Harshal Tupe <harshaltupe12@gmail.com>
2022-08-08 10:44:07 +0530

    data was suceessfully fetch but unable to render. it was printing on console

--- src/pages/TrendingPortal/Trending.js
+++ src/pages/TrendingPortal/Trending.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import TrendingIntro from "../../components/TrendingInro";
 import NewCard from "../../components/NewCard";
+import { ref as dbref, child, get } from "firebase/database";
+import { database } from "../../firebase/init-firebase";
 
 function Trending() {
+  const [data, setData] = useState([]);
+
+  function getAllReports() {
+    const db = dbref(database);
+    get(child(db, `/reportsList/`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          let data = snapshot.val();
+          let allData = new Array();
+          Object.keys(data).forEach((key) => {
+            allData.push(data[key]);
+          });
+          // setState here
+          //ex : setData(allData)
+          console.log(allData);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  useEffect(() => {
+    return () => {
+      getAllReports();
+    };
+  }, []);
+
   return (
     <div className="parent-section darkMode">
       <TrendingIntro />
       <div className="flex m-8">
-        <NewCard />
+      {data.map((ele) => {
+        console.log(ele)
+                return(
+                <NewCard date={ele.date} name={ele.name} key={ele.id}/>
+                )
+              })}
       </div>
     </div>
   );
