42488ecd2aff58ad9df2f2a1bf04e5aeb180e7e8
Harshal Tupe <harshaltupe12@gmail.com>
2022-08-14 17:38:06 +0530

    TrendingGraph was created for displaying Graphs..

---
+++ src/pages/Dashboard/TrendingGraph.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function TrendingGraph() {
+  return (
+    <div>Graph Here</div>
+    // if you want to test this page then add this page on Dashboard.js for testing purposse only and remove while commiting changes....
+  )
+}
