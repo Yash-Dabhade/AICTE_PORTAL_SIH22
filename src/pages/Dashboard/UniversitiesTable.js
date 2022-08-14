06a25c8dfe1e80f13e4e74c4cdf48927c78ce7cc
Harshal Tupe <harshaltupe12@gmail.com>
2022-08-14 17:39:05 +0530

    UniversitiesTable.js file created for displaying Universities

---
+++ src/pages/Dashboard/UniversitiesTable.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function UniversitiesTable() {
+  return (
+    <div>Table Here</div>
+    // if you want to test this page then add this page on Dashboard.js for testing purposse only and remove while commiting changes....
+  )
+}
