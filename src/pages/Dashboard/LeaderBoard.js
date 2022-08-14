8fa59c46f41b641eb32da3946de4647da3e40038
Harshal Tupe <harshaltupe12@gmail.com>
2022-08-14 17:37:17 +0530

    LeaderBoard.js created for showing Experts information

---
+++ src/pages/Dashboard/LeaderBoard.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function LeaderBoard() {
+  return (
+    <div>LeaderBoard here</div>
+    // if you want to test this page then add this page on Dashboard.js for testing purposse only and remove while commiting changes....
+  )
+}
