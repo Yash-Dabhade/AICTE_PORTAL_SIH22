7332aaeb52b03f3f7ef20287a775224e6d3bc607
Harshal Tupe <harshaltupe12@gmail.com>
2022-08-15 18:08:35 +0530

    tablw was done with static information

--- src/pages/Dashboard/UniversitiesTable.js
+++ src/pages/Dashboard/UniversitiesTable.js
@@ -2,7 +2,81 @@ import React from 'react'
 
 export default function UniversitiesTable() {
   return (
-    <div>Table Here</div>
+    <div className='mt-11 shadow-2xl rounded-2xl border-solid border-2 border-black-900 ' style={{backgroundColor: "white"}}>
+<div class="flex flex-col">
+  <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+      <div class="overflow-hidden ">
+        <table class="min-w-full border text-center">
+          <thead class="border-b">
+            <tr>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+              Universities
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                Percentage
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="border-b">
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">1</td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                MSBTE
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                60%
+              </td>
+            </tr>
+
+            <tr class="border-b">
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">2</td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                LPU
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                56%
+              </td>
+            </tr>
+
+            <tr class="border-b">
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">3</td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                SPPU
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                53%
+              </td>
+            </tr>
+
+            <tr class="border-b">
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">4</td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+               BATU
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+               51%
+              </td>
+            </tr>
+            <tr class="border-b">
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">5</td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                SEA
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                48%
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
     // if you want to test this page then add this page on Dashboard.js for testing purposse only and remove while commiting changes....
   )
 }
