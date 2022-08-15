931de90d3666bec2fc9557189faef223a4af5463
Harshal Tupe <harshaltupe12@gmail.com>
2022-08-15 18:07:47 +0530

    leader board was done

--- src/pages/Dashboard/LeaderBoard.js
+++ src/pages/Dashboard/LeaderBoard.js
@@ -1,8 +1,62 @@
 import React from 'react'
 
 export default function LeaderBoard() {
+  
+  const demo = [
+    {
+      index : '1st',
+      title : "Harry",
+      desc : "60"
+
+    },
+    {
+      index : '2nd',
+      title : "Tonny Stark",
+      desc : "56"
+
+    },
+    {
+      index : '3rd',
+      title : "Elon Musk",
+      desc : "39"
+
+    },
+    {
+      index : '4th',
+      title : "Odin",
+      desc : "41"
+
+    },
+    {
+      index : '5th',
+      title : "Thor",
+      desc : "49"
+
+    }
+  ]
+
+  const card = demo.map((ele)=> {
+    return (
+    <div className="max-w-sm rounded-2xl overflow-hidden shadow-xl" style={{backgroundColor: "#ddddeb"}}>
+    <div className="px-6 py-4 ">
+      <p className='text-slate-400'>{ele.index}</p>
+      <div className="font-bold text-xl mb-2">{ele.title}</div>
+      <p className="text-slate-700 text-base">
+        Score : {ele.desc} 
+      </p>
+    </div>
+  </div>
+    )
+  })
+
   return (
-    <div>LeaderBoard here</div>
+    <div className="">
+
+      <div className="grid grid-cols-5 gap-5 ">
+        {card}
+      </div>
+
+    </div>
     // if you want to test this page then add this page on Dashboard.js for testing purposse only and remove while commiting changes....
   )
 }
