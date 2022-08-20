38b3f2dfb9739a3c6cf88942f5641ee8b0ee0497
Snehal Patil <patilonlysnehal2003@gmail.com>
2022-08-20 11:24:22 +0530

    404, 500 and unauthorised access page

---
+++ src/pages/Authentication/Unauthorised.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Unauthorised = () => {
+    return (
+        <div class="container ml-10 mt-40 py-20 px-20 bg-slate-100">
+        <a href="#" class="bg-slate-100">
+            <p class="text-2xl font-bold">No Authorization found.</p>
+            <p class="text-xl">This page is not publically available.</p>
+        </a>
+    </div>
+    );
+    
+ };
+
+ export default Unauthorised;
